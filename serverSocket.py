5af266c49a9463b26be61adaea20f5d2d52494ca
vahidtwo <vahidtwo@gmail.com>
2019-01-08 22:50:44 +0330

    some changing in algoritme

--- serverSocket.py
+++ serverSocket.py
@@ -1,46 +1,48 @@
+#! /usr/bin/python
 import socket
 import RPi.GPIO as gpio 
 import time
 listensocket =socket.socket(socket.AF_INET,socket.SOCK_STREAM)
 port = 16662
 IP = "192.168.88.225"
-pin=32
+pin=7
 gpio.setmode(gpio.BOARD)
-gpio.setup(pin,gpio.OUT)
 try:
 	listensocket.bind((IP,port))
 	listensocket.listen(999)
+        connectionStatus =False
+        
 	print("server started "+IP+" on port "+ str(port))
-	#clientsocket , add= listensocket.accept()
-	print("new connection made")
-	#pin=32
-	#gpio.setmode(gpio.BOARD)
-	#gpio.setup(pin,gpio.OUT)
-        for i in range(0,3):
-            gpio.output(pin,True)
-	    time.sleep(1)
-	    gpio.output(pin,False)
-	print("setup")
 	while True:
 		clientsocket , add= listensocket.accept()
-		#print("new connection made")
-		print("in while")
-		message =clientsocket.recv(7)
+		message =clientsocket.recv(8)
 		print("message is : ", message )
-		#time.sleep(1)
 		if message == "end":
 			print("the end")
 			#clientsocket.clese()
 		if message != "":			
-			if message =="true":
-				gpio.output(pin,True)
-			else:
-				print(message)
-				gpio.output(pin,False)
-				#clientsocket.close()
-				#running=False 
+			if message =="false":
+                            gpio.setup(pin,gpio.OUT)
+                            connectionStatus=True
+                        elif message=="true":
+                            connectionStatus=False    
+                            gpio.setup(pin,gpio.IN)
+			    print(message)
+                        elif message=="getstate":
+                            try:
+                                sendsocket =socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+                                print("sendsocket creat",add[0])
 
-except:
+                                sendsocket.connect((str(add[0]),16662))
+                                print("connect",add[0])
+                                sendsocket.send("connectionStatus:"+str(connectionStatus))
+                                sendsocket.close()
+                                print("send")
+                            except:
+                                print("not send")
+
+except Exception as e :
 	gpio.cleanup()
 	print("cleanUP")
+        print(e)
 	listensocket.close()
